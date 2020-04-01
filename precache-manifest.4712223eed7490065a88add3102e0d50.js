d4c7d8de02e378fb45a14a620a3717466ff5393b
DeviousCuttlefish <57159977+DeviousCuttlefish@users.noreply.github.com>
2020-04-01 12:28:14 -0600

    Updates

---
+++ precache-manifest.4712223eed7490065a88add3102e0d50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a161d59afe4f7649417c46caf27e5ada",
+    "url": "/ReactApp-Todo/index.html"
+  },
+  {
+    "revision": "0962275b1d6032ea2306",
+    "url": "/ReactApp-Todo/static/css/main.7c8f2f72.chunk.css"
+  },
+  {
+    "revision": "2b8c8c034590907b82bc",
+    "url": "/ReactApp-Todo/static/js/2.1e1e04c9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactApp-Todo/static/js/2.1e1e04c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0962275b1d6032ea2306",
+    "url": "/ReactApp-Todo/static/js/main.06de4783.chunk.js"
+  },
+  {
+    "revision": "64c0a320188f993fda25",
+    "url": "/ReactApp-Todo/static/js/runtime-main.2dfba4c5.js"
+  }
+]);
